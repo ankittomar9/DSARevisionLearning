fb77d5d3137845c9460a4dc9ce9603a9b2321858
tomar <tomarankit78@gmail.com>
2026-01-15 19:13:42 +0530

    revision Programs Day[3]

---
+++ arrays/Q_19_Buy_Sell_Stock.java
@@ -0,0 +1,27 @@
+package DSARevisionLearning.arrays;
+
+public class Q_19_Buy_Sell_Stock {
+    //Given prices array, find max profit: buy on one day, sell on later day.
+        //If no profit possible → return 0.
+    public static void main(String args[]){
+        int arr[]={7,1,5,3,6,4};
+            // arr[] can also be defined as price[]
+        int minPrice=arr[0];
+            int max_Profit=0;
+            int profit_today=0;
+
+            for(int i=0;i<arr.length;i++){
+              
+                profit_today=arr[i]-minPrice;
+
+                max_Profit=Math.max(max_Profit, profit_today);
+               
+                minPrice=Math.min(minPrice, arr[i]);
+            }
+
+              System.out.println("min Price: "+minPrice);
+            System.out.println("Maximum Profit  : "+max_Profit);
+
+    }
+    
+}
