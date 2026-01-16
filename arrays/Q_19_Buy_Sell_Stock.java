c96a18d326d1bb39536f2aece224f93bac356a5a
tomar <tomarankit78@gmail.com>
2026-01-16 23:02:59 +0530

    revision Programs Day[4]

--- arrays/Q_19_Buy_Sell_Stock.java
+++ arrays/Q_19_Buy_Sell_Stock.java
@@ -5,19 +5,16 @@ public class Q_19_Buy_Sell_Stock {
         //If no profit possible → return 0.
     public static void main(String args[]){
         int arr[]={7,1,5,3,6,4};
-            // arr[] can also be defined as price[]
+        // arr[] can also be defined as price[] also arr is buying prices
         int minPrice=arr[0];
-            int max_Profit=0;
-            int profit_today=0;
+        int max_Profit=0;
 
-            for(int i=0;i<arr.length;i++){
-              
-                profit_today=arr[i]-minPrice;
+        for(int i=0;i<arr.length;i++){  
+            int profit_today=arr[i]-minPrice;
+            max_Profit=Math.max(max_Profit, profit_today);
+            minPrice=Math.min(minPrice, arr[i]);
+        }
 
-                max_Profit=Math.max(max_Profit, profit_today);
-               
-                minPrice=Math.min(minPrice, arr[i]);
-            }
 
               System.out.println("min Price: "+minPrice);
             System.out.println("Maximum Profit  : "+max_Profit);
