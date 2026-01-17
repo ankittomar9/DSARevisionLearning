2ab0afb458b2a44d0e6daff924eb88b1b40561eb
tomar <tomarankit78@gmail.com>
2026-01-17 22:44:56 +0530

    revision Programs Day[4]

---
+++ arrays/Q_43_Subarray_Sum_SW_Optimized.java
@@ -0,0 +1,19 @@
+package DSARevisionLearning.arrays;
+
+public class Q_43_Subarray_Sum_SW_Optimized {
+    public static void main(String args[]){
+        int arr[]={};
+        int n=arr.length;
+        int k=3;
+        int currentSum=0;
+        //step 1: calculate the sum for 1st window
+        for(int i=0;i<k;i++){
+            currentSum=currentSum+arr[i];
+        }
+
+
+
+
+    }
+    
+}
