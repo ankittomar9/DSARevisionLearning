bc4a060eedda564ba8e5923b9bf1fc244d50c473
tomar <tomarankit78@gmail.com>
2026-01-18 16:42:52 +0530

    revision Programs Day[5]

--- arrays/Q_43_Subarray_Sum_SW_Optimized.java
+++ arrays/Q_43_Subarray_Sum_SW_Optimized.java
@@ -1,19 +1,21 @@
 package DSARevisionLearning.arrays;
-
 public class Q_43_Subarray_Sum_SW_Optimized {
     public static void main(String args[]){
-        int arr[]={};
-        int n=arr.length;
-        int k=3;
-        int currentSum=0;
+        int arr[]= {2, 1, 5, 1, 3, 2};        int n=arr.length;    int k=3;
+        if(n==0){        System.out.println("Array is Empty");  return;        }    
         //step 1: calculate the sum for 1st window
+          int currentSum=0;
         for(int i=0;i<k;i++){
             currentSum=currentSum+arr[i];
         }
+        int maxSum=currentSum;
 
+        for(int i=k;i<n;i++){
+            currentSum=currentSum+arr[i]-arr[i-k];
 
-
-
+            maxSum=Math.max(currentSum, maxSum);
+        }
+        System.out.println("maximum subarray sum of window k is :"+maxSum);
     }
     
 }
