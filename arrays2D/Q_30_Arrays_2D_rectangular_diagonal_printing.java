42a91c8c05a56983467a91fa4903b98b800f7ea3
tomar <tomarankit78@gmail.com>
2026-01-18 16:43:06 +0530

    revision Programs Day[5]

---
+++ arrays2D/Q_30_Arrays_2D_rectangular_diagonal_printing.java
@@ -0,0 +1,13 @@
+package DSARevisionLearning.arrays2D;
+
+public class Q_30_Arrays_2D_rectangular_diagonal_printing {
+    public static void main(String args[]){
+        int matrix[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        int n=matrix.length;
+        int m=matrix[0].length;
+
+        
+
+    }
+    
+}
