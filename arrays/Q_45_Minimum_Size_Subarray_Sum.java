4fab0cba97fc406650553b851a2acb2858027aa1
tomar <tomarankit78@gmail.com>
2026-01-19 18:51:14 +0530

    revision Programs Day[5]

--- arrays/Q_45_Minimum_Size_Subarray_Sum.java
+++ arrays/Q_45_Minimum_Size_Subarray_Sum.java
@@ -1,5 +1,4 @@
 package DSARevisionLearning.arrays;
-
 public class Q_45_Minimum_Size_Subarray_Sum {
     public static void main(String args[]){
         /* Given an array of positive integers and a target value, find the minimal length of a 
@@ -8,7 +7,6 @@ public class Q_45_Minimum_Size_Subarray_Sum {
         int arr[]={2,3,1,2,4,3}; int target=7;
             int n=arr.length; int min_Length=Integer.MAX_VALUE;
         if(n==0){System.out.print("-1");return;}
-
         int left=0; int currentSum=0;
         for(int right=0;right<n;right++){
             currentSum=currentSum+arr[right];
@@ -18,16 +16,11 @@ public class Q_45_Minimum_Size_Subarray_Sum {
                 currentSum=currentSum-arr[left];
                 left++;
             }
-        } 
-        
+        }  
         if(min_Length==Integer.MAX_VALUE){
                System.out.print("0");
         }else{
-                System.out.println("Minimum Length subarray of target is :\n"+min_Length);
+ System.out.println("Minimum Length subarray of target is :\n"+min_Length);
         }
-     
-
-
+         }
     }
-    
-}
