31f66a6c103c34a19ec11223e1e0737eeb3b7a79
tomar <tomarankit78@gmail.com>
2026-01-20 18:58:24 +0530

    DSA Revision Programs

---
+++ DSARevisionLearning/arrays/Q_47_Remove_0_to_Max_Subarray_length.java
@@ -0,0 +1,17 @@
+package DSARevisionLearning.arrays;
+
+public class Q_47_Remove_0_to_Max_Subarray_length {
+    public static void main(String args[]){
+        /*Problem: Longest Subarray of 1's After Deleting One Element
+          Goal: Return the length of the longest subarray of 1s assuming you MUST delete one element. 
+            Input: nums = [0,1,1,1,0,1,1,0,1]  Output: 5
+            Explanation: After deleting the number in position 4, 
+        [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+            */
+        
+        
+  
+  
+    }
+    
+}
