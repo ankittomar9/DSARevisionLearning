3b2ef895abec268ce27a5effb06bd0c9192ccaf4
tomar <tomarankit78@gmail.com>
2026-01-24 22:31:39 +0530

    DSA Revision Programs

---
+++ dsa_revision_miscellaneous/hashmap/Q_7_Intersection_of_Two_Arrays.java
@@ -0,0 +1,28 @@
+package dsa_revision_miscellaneous.hashmap;
+
+import java.util.HashSet;
+
+public class Q_7_Intersection_of_Two_Arrays {
+    public static void main(String args[]){
+         int arr1[]={4,9,5};
+        int arr2[]={9,4,9,8,4};
+        if(arr1 ==null || arr2==null){
+            return;
+        }
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<arr1.length;i++){
+            set.add(arr1[i]);
+        }   
+        System.out.println(set);
+
+        System.out.println("Intersection Elements : ");
+            for(int i=0;i<arr2.length;i++){
+                if(set.contains(arr2[i])){
+                         System.out.print(arr2[i]+" ");
+                    set.remove(arr2[i]);
+                }
+            }
+           
+    }
+    
+}
