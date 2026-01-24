041f3eb3758f35865a270d09b70a1aca62b85617
tomar <tomarankit78@gmail.com>
2026-01-24 21:27:40 +0530

    DSA Revision Programs

--- DSARevisionLearning/hashmap/Q_6_Find_First_Non_Repeating_Element.java
+++ DSARevisionLearning/hashmap/Q_6_Find_First_Non_Repeating_Element.java
@@ -1,8 +1,32 @@
 package DSARevisionLearning.hashmap;
-
+import java.util.HashMap;
 public class Q_6_Find_First_Non_Repeating_Element {
     public static void main(String args[]){
-        
+        int arr[]={9,6,9,7,6,4,7};
+        boolean isFound=false;
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }else{
+                map.put(arr[i], 1);
+            }
+        }
+         System.out.println("Frequency Map: " + map);
+
+        for(int i=0;i<arr.length;i++){
+            if(map.get(arr[i])==1){
+                 System.out.println("First non-repeating element is: " + arr[i]);
+                isFound=true;
+                break;
+            }
+        }
+        if(!isFound){
+             System.out.println("No first non repeating elements"); 
+        }
+
     }
     
 }
