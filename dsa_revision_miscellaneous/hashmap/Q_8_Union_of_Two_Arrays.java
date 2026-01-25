9b7d939c451b2c4f9bb47b67aee2c1e7580d6415
tomar <tomarankit78@gmail.com>
2026-01-25 22:36:18 +0530

    DSA Revision Programs

---
+++ dsa_revision_miscellaneous/hashmap/Q_8_Union_of_Two_Arrays.java
@@ -0,0 +1,13 @@
+package dsa_revision_miscellaneous.hashmap;
+
+public class Q_8_Union_of_Two_Arrays {
+    public static void main(String args[]){
+         int arr1[]={7,3,9};
+        int arr2[]={6,3,9,2,2,4};
+         if (arr1 == null || arr2 == null) {  return;    }
+
+            
+
+    }
+    
+}
