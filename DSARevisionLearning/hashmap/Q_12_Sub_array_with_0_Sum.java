3ea80c4a4c89bf51b1a14bf185321e8118304580
tomar <tomarankit78@gmail.com>
2026-01-28 22:07:28 +0530

    New Solved Questions

--- DSARevisionLearning/hashmap/Q_12_Sub_array_with_0_Sum.java
+++ DSARevisionLearning/hashmap/Q_12_Sub_array_with_0_Sum.java
@@ -1,15 +1,12 @@
 package DSARevisionLearning.hashmap;
-
 import java.util.HashMap;
-
 public class Q_12_Sub_array_with_0_Sum {
     public static void main(String args[]){
         int arr[]={4,2,-3,1,6};
         int n=arr.length; boolean isFound=false;
         //expected output is : 2+(-3)+1=0 so true; 
         HashMap<Integer,Integer> map=new HashMap<>();
-            int currentSum=0; //int maxSum=Integer.MIN_VALUE;
-            
+            int currentSum=0;             
             for(int i=0;i<arr.length;i++){
                 currentSum=currentSum+arr[i];
 
@@ -25,12 +22,10 @@ public class Q_12_Sub_array_with_0_Sum {
                                    + (map.get(currentSum) + 1) + " and " + i);                    
                 }
                 map.put(currentSum, i);
-
             }
 
         if (!isFound) {
             System.out.println("No subarray with 0 sum found");
         }
     }
-    
 }
