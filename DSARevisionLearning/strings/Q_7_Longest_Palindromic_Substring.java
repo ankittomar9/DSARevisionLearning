ed1d8ac55606b4ceda784189a3d7eb7df31da373
tomar <tomarankit78@gmail.com>
2026-01-28 22:07:42 +0530

    New Solved Questions

---
+++ DSARevisionLearning/strings/Q_7_Longest_Palindromic_Substring.java
@@ -0,0 +1,33 @@
+package DSARevisionLearning.strings;
+
+public class Q_7_Longest_Palindromic_Substring {
+    static int maxLen=0;// tracks longest length
+    static int start=0; // tracks where palindrome start
+    public static void main(String args[]){
+        String s="babad";
+             maxLen = 0;
+            start = 0;
+            for(int i=0;i<s.length();i++){
+
+                 expandHelper(s,i,i);// for ODD
+                 expandHelper(s, i, i + 1); // for even
+            }
+            System.out.println("Longest Palindrome: " + s.substring(start, start + maxLen));
+       
+    }
+
+    public static void  expandHelper(String s,int left ,int right){
+        while(left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)){
+
+            left--;right++;
+        }
+        int currentLen=right-left-1;
+
+        if (currentLen > maxLen) {
+            maxLen = currentLen;
+            start = left + 1;
+        }
+
+    }
+    
+}
