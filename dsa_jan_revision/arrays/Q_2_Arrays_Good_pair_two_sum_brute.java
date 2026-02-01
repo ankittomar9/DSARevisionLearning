328e8a4fcd9aa47c3c5c1d570bc0014978892d10
tomar <tomarankit78@gmail.com>
2026-02-01 16:29:30 +0530

    New Solved Questions

---
+++ dsa_jan_revision/arrays/Q_2_Arrays_Good_pair_two_sum_brute.java
@@ -0,0 +1,9 @@
+package dsa_jan_revision.arrays;
+
+public class Q_2_Arrays_Good_pair_two_sum_brute {
+    public static void main(String args){
+        int arr[]={1,4,2,3,6,8,4}; int target=7;
+        
+    }
+    
+}
