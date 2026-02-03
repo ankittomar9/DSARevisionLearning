8802da001aa1bca438339f0f27118ef8de9c168c
tomar <tomarankit78@gmail.com>
2026-02-03 22:36:45 +0530

    New Solved Questions

---
+++ DSARevisionLearning/strings/Q_3_Toggle_case_Lower_Upper_case.java
@@ -0,0 +1,28 @@
+package DSARevisionLearning.strings;
+
+public class Q_3_Toggle_case_Lower_Upper_case {
+    public static void main(String args[]){
+        String s1="ankit_doing_JAVA_DSA_lEARNINING";
+          System.out.println("Original String \n"+s1);
+          if(s1==null || s1.length()==0){
+            System.out.println("Empty String or Null String");return;
+        }
+         char arr[]=s1.toCharArray();
+
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>='a' && arr[i]<='z'){
+                arr[i]=(char)(arr[i]-32);
+            }else if(arr[i]>='A' && arr[i]<='Z') {
+                arr[i]=(char)(arr[i]+32);    
+            }
+        }
+           String s2=new String(arr);
+            System.out.println("\nModified String \n"+s2);
+    }
+    
+}
+
+/*TC: O(N)
+  SC:O(N) because of char array even if we used string builder it would have been same
+*/
