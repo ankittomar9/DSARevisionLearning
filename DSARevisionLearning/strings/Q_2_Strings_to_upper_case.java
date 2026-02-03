2f4a1b29576fa79d967e61c35a9702a02215ac9a
tomar <tomarankit78@gmail.com>
2026-02-03 22:36:40 +0530

    New Solved Questions

--- DSARevisionLearning/strings/Q_2_Strings_to_upper_case.java
+++ DSARevisionLearning/strings/Q_2_Strings_to_upper_case.java
@@ -1,13 +1,29 @@
 package DSARevisionLearning.strings;
-
 public class Q_2_Strings_to_upper_case {
-    public static void main(String args[]){
-        String s1="AMAN_LEARNS_JAVA_STREAMS";
-        
+    public static void main(String args[]){ 
+         /*Goal  is to convert the UpperCase string to lowerCase */
+         //String are immutable so we need to convert it to char array or
+        //Use String Builder (here we won't use string builder)
+        String s1="aman_learns_java_streams";
+        System.out.println("Original String \n"+s1);
+        if(s1==null || s1.length()==0){
+            System.out.println("Empty String or Null String");return;
+        }
+        char arr[]=s1.toCharArray();
 
-       
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>='a' && arr[i]<='z'){
+                arr[i]=(char)(arr[i]-32);
+            }
+        }
+        String s2=new String(arr);
 
+        System.out.println("\nConverted to upperCase \n"+s2);
 
-    }
-    
+    }  
 }
+
+
+/*TC: O(N)
+  SC:O(N) because of char array even if we used string builder it would have been same
+*/
