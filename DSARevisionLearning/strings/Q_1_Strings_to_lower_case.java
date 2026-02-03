3f10e07a35769734ed052b3a6e44543434412101
tomar <tomarankit78@gmail.com>
2026-02-03 22:36:38 +0530

    New Solved Questions

--- DSARevisionLearning/strings/Q_1_Strings_to_lower_case.java
+++ DSARevisionLearning/strings/Q_1_Strings_to_lower_case.java
@@ -1,22 +1,30 @@
 package DSARevisionLearning.strings;
 
-import java.util.Arrays;
-
 public class Q_1_Strings_to_lower_case {
     public static void main(String args[]){
-        String s1="ankit_learns_programming";
-          System.out.println("Original string \n"+s1);
-        char arr[]=s1.toCharArray();
+      /*Goal  is to convert the UpperCase string to lowerCase */
+        String s1="AMAN_KNOWS_JAVA_VERY_WELL";
+          System.out.println("Original String\n"+s1);
+        //String are immutable so we need to convert it to char array or
+        //Use String Builder (here we won't use string builder)
+        if(s1==null || s1.length()==0  ){
+            System.out.println("String is Empty  or null");return;
+        }
 
+        char arr[]=s1.toCharArray();
         for(int i=0;i<arr.length;i++){
-            if(arr[i]>='a' && arr[i]<='z'){
-                arr[i]=(char)(arr[i]-32);
+            if(arr[i]>='A' && arr[i]<='Z'){
+              arr[i]=(char)(arr[i]+32);
             }
         }
         String s2=new String(arr);
-        System.out.println("Converted string \n"+s2);
 
-        System.out.println(Arrays.toString(arr));
+        System.out.print("\nConverted to lowerCase\n"+s2);
+
     }
     
 }
+
+/*TC: O(N)
+  SC:O(N) because of char array even if we used string builder it would have been same
+*/
