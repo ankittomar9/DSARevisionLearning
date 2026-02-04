de0abc5679adc6dc6ee8d11b706ad8c8472c1c9e
tomar <tomarankit78@gmail.com>
2026-02-04 11:45:07 +0530

    New Solved Questions

---
+++ DSARevisionLearning/recursion/Q_2_Print_1_to_N_Numbers.java
@@ -0,0 +1,17 @@
+package DSARevisionLearning.recursion;
+
+public class Q_2_Print_1_to_N_Numbers {
+    public static void main(String args[]){
+        int n=10;
+        printHelper_1_to_N_helper(n);
+    }
+    public static void printHelper_1_to_N_helper(int n){
+        if(n<=0){ //edge case
+            return;
+        }    
+        
+        printHelper_1_to_N_helper(n-1);
+        System.out.print(n+" ");
+
+    }   
+}
