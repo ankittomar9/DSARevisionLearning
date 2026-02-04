120dc25ead1dc1da13ff5ce775d670164b484e31
tomar <tomarankit78@gmail.com>
2026-02-04 17:41:42 +0530

    New Solved Questions

--- DSARevisionLearning/recursion/Q_6_Reverse_Number_Using_Recursion_v2.java
+++ DSARevisionLearning/recursion/Q_6_Reverse_Number_Using_Recursion_v2.java
@@ -1,23 +1,25 @@
 package DSARevisionLearning.recursion;
-
 public class Q_6_Reverse_Number_Using_Recursion_v2 {
+    /*/*Reverse a Number using Recursion Best Practice using Accumulator */
     public static void main(String args[]){
         long n=12345;
           System.out.println("Original number : "+n);
         //expected output  54321
-        long result=reverseHelper(n);
+        long result=reverseHelper(n,0); 
+        // to reach the numbers place like ones tens hundred thousand and ten Thousand
 
         System.out.println("Reversed number : "+result);
     }
-    public static long reverseHelper(long n){
-        if(n==0){            return 0;    }
+    public static long reverseHelper(long n,long currentReverse){
+                 if(n==0){  
+                 return currentReverse;    }
 
             long extract=n%10;
-            long reverse=extract*10;
+            long newReverse=(currentReverse*10)+extract;
             
             long reduce=n/10;
 
-            return reverse;
+            return reverseHelper(reduce,newReverse);
     }
     
 }
