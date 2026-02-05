90e66ba7f04539772903b808d9846dc47b02291b
tomar <tomarankit78@gmail.com>
2026-02-05 23:15:28 +0530

    New Solved Questions

--- DSARevisionLearning/recursion/Q_7_Check_if_array_is_sorted_or_not.java
+++ DSARevisionLearning/recursion/Q_7_Check_if_array_is_sorted_or_not.java
@@ -1,32 +1,27 @@
 package DSARevisionLearning.recursion;
-
 public class Q_7_Check_if_array_is_sorted_or_not {
     public static void main(String args[]){
         //int arr[] = {1,2,3,4,5};
-        int arr[]={1,2,4,3};
-        int index=0;
-        boolean result=isArraySortedHelper(arr,0);
-
+        int arr[]={1,2,4,3}; 
+         int index=0; // to check if array is sorted we need to check arr[i]>arr[i+1]
+        boolean result=isArraySorted(arr,index);
         if(result){
             System.out.println("Array is Sorted");
         }else{
-            System.out.println("Array is Not Sorted");
-        }
-        
+             System.out.println("Array is not  Sorted");        }
     }
-    public static boolean isArraySortedHelper(int arr[],int index){
-        boolean isCheck=false;
-       
+      public static boolean isArraySorted(int arr[],int index){   
         //base case
-        if(index == arr.length-1){
+        if(index ==arr.length-1){
             return true;
         }
-
-        //main logic
+        //Main logic
         if(arr[index]>arr[index+1]){
             return false;
         }
-        return isArraySortedHelper(arr,index+1);
-    }
-    
+        return isArraySorted(arr,index+1);
+     }   
 }
+/*TC: O(N)
+  SC:O(N) 
+*/
