59dd33b0a4cac7b216105ca4a3eb10ffcb1864bd
tomar <tomarankit78@gmail.com>
2026-02-06 23:42:31 +0530

    New Solved Questions

--- DSARevisionLearning/recursion/Q_11_Power_Function_Iterative.java
+++ DSARevisionLearning/recursion/Q_11_Power_Function_Iterative.java
@@ -2,20 +2,15 @@ package DSARevisionLearning.recursion;
 
 public class Q_11_Power_Function_Iterative {
     public static void main(String args[]){
-        long a=2 ;long n=5;  //expected output is a^b = 2^5=32
-
-        long result=powerHelper(a,n);
-        System.out.println("Power of a is  "+result);
-    }
-    public static long powerHelper(long a,long n){
-        long power=1;
-        long ans=0;
-
+         int a=10;  int n=2;
+        //Expected a^n which is 10^2 ==100
+        long ans=1;
         for(int i=0;i<n;i++){
-            ans=ans*power;
-            power=power*n;
+            ans=ans*a;
         }
-        return ans;
+
+        System.out.println("Power of a^n is \n"+ans);
+    
     }
     
 }
