7fd0cc594ebaf89510fb40dcb66592826153f2c0
tomar <tomarankit78@gmail.com>
2026-02-06 19:23:25 +0530

    New Solved Questions

--- DSARevisionLearning/recursion/Q_9_Binary_Search_using_recursion.java
+++ DSARevisionLearning/recursion/Q_9_Binary_Search_using_recursion.java
@@ -2,35 +2,35 @@ package DSARevisionLearning.recursion;
 public class Q_9_Binary_Search_using_recursion {
     public static void main(String args[]){
         int arr[]={1,3,4,6,7}; 
-         int low=0; int high=arr.length-1;
-        int target=9;// false case we can also check for target=6;
-       int result= binarySearchHelper(arr,target,low,high);
+        int target=4; int low=0;int high=arr.length-1;
+        int result=binaryHelper(arr,target,low,high);
+
         if(result!=-1){
-            System.out.println("Target found at "+result);
+            System.out.println("Target Found at "+result);
         }else{
-            System.out.println("Target doesn't exists");
+              System.out.println("Target Not Exists ");
         }
     }
-    public static int  binarySearchHelper(int arr[],int target,int low,int high){
-        
-        if(low>high){  //base case
+    public static int binaryHelper(int arr[],int target,int low,int high){
+        //base case
+        if(low>high){
             return -1;
         }
-        //Main Logic 
+        //Main Logic
         int mid=low+(high-low)/2;
 
         if(arr[mid]==target){
-            return mid;
+            return mid;       
         }
         if(arr[mid]<target){
-             return binarySearchHelper(arr,target,mid+1,high);
+            return binaryHelper(arr, target, mid+1, high);
         }else{
-             return binarySearchHelper(arr,target,low,mid-1);
-        }
-    }
+            return binaryHelper(arr, target, low, mid-1);
+        } 
+     }
 }
 /**
  * TC: O(logN)
-    SC:O(N) Call stack (Number of calls )
+    SC:O(logN) Call stack (Number of calls )
 
 */
