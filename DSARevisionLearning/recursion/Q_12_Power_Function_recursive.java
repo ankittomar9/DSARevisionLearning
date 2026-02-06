800ed12217ed918652f3fe14136700e0d3ad02e4
tomar <tomarankit78@gmail.com>
2026-02-06 23:42:34 +0530

    New Solved Questions

--- DSARevisionLearning/recursion/Q_12_Power_Function_recursive.java
+++ DSARevisionLearning/recursion/Q_12_Power_Function_recursive.java
@@ -1,5 +1,20 @@
 package DSARevisionLearning.recursion;
 
 public class Q_12_Power_Function_recursive {
-    
+    public static void main(String args[]){
+        int a=2; int n=5;
+        
+        long result=powerHelper(a,n);
+        System.out.println("Power of a^n is : \n"+result);
+
+    }
+
+    public static long powerHelper(int a,int n){
+        if(n==0){
+            return 1;
+        }
+
+        long result=powerHelper(a, n-1)*a;
+        return result;
+    }
 }
