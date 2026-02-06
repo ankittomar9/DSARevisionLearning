8762f7bd194128dcd9344b4d28957cab8437e4d2
tomar <tomarankit78@gmail.com>
2026-02-06 18:23:54 +0530

    New Solved Questions

--- DSARevisionLearning/recursion/Q_9_Binary_Search_using_recursion.java
+++ DSARevisionLearning/recursion/Q_9_Binary_Search_using_recursion.java
@@ -1,11 +1,15 @@
 package DSARevisionLearning.recursion;
-
 public class Q_9_Binary_Search_using_recursion {
     public static void main(String args[]){
         int arr[]={1,3,4,6,7}; 
          int low=0; int high=arr.length-1;
         int target=9;// false case we can also check for target=6;
-        binarySearchHelper(arr,target,low,high);
+       int result= binarySearchHelper(arr,target,low,high);
+        if(result!=-1){
+            System.out.println("Target found at "+result);
+        }else{
+            System.out.println("Target doesn't exists");
+        }
     }
     public static int  binarySearchHelper(int arr[],int target,int low,int high){
         
@@ -23,8 +27,10 @@ public class Q_9_Binary_Search_using_recursion {
         }else{
              return binarySearchHelper(arr,target,low,mid-1);
         }
-
-       
     }
-    
 }
+/**
+ * TC: O(logN)
+    SC:O(N) Call stack (Number of calls )
+
+*/
