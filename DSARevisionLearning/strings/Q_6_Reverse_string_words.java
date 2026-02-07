05e6e44197f4cc6bd4a7310f7f70d636727e4a08
tomar <tomarankit78@gmail.com>
2026-02-07 22:26:48 +0530

    New Solved Questions

---
+++ DSARevisionLearning/strings/Q_6_Reverse_string_words.java
@@ -0,0 +1,38 @@
+package DSARevisionLearning.strings;
+import java.util.Arrays;
+public class Q_6_Reverse_string_words {
+    public static void main(String args[]){
+           String s1="the sky is blue";
+           System.out.println("Original String : \n"+s1);
+        char arr[]=s1.toCharArray();
+        int start=0; int end=arr.length-1;
+
+        reverseStringHelper(arr,start,end);
+         System.out.println(Arrays.toString(arr)); 
+
+         for(int i=0;i<arr.length;i++){
+            if(arr[i]==' '){
+                  reverseStringHelper(arr,start,i-1);
+                  start=i+1;
+            }
+            else if( i== end){
+                 reverseStringHelper(arr,start,end);
+            }
+         }
+         System.out.println("Final Result: " + new String(arr));
+      
+        }
+        public static void reverseStringHelper(char arr[],int start,int end){
+            int i=start;int j=end;
+           while(i<j){
+            char temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+           } 
+            
+           
+        }
+    
+}
