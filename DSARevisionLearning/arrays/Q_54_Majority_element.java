50fd4f945412a9dcd70d3db05b30f36cf7b7ae1a
tomar <tomarankit78@gmail.com>
2026-02-10 21:05:30 +0530

    New Solved Questions

---
+++ DSARevisionLearning/arrays/Q_54_Majority_element.java
@@ -0,0 +1,11 @@
+package DSARevisionLearning.arrays;
+
+public class Q_54_Majority_element {
+    public static void main(String args[]){
+         int arr[]={2,2,1,1,1,2,2}; // n=7 majority is n/2 which is 7/2= 3.5 =3
+        //This questions solved by Moore Voting algorithm
+
+        
+    }
+    
+}
