550946ac61036f2d218e07a8314932c5254bc1e7
tomar <tomarankit78@gmail.com>
2026-02-13 05:03:15 +0530

    New Solved Questions

--- DSARevisionLearning/sorting/Q_5_Merge_Sort.java
+++ DSARevisionLearning/sorting/Q_5_Merge_Sort.java
@@ -1,9 +1,11 @@
 package DSARevisionLearning.sorting;
-
+import java.util.Arrays;
 public class Q_5_Merge_Sort {
     public static void main(String args[]){
-        int arr[]={5,1,4,2,8,6,3};
+        int arr[]={5,1,4,2,8,6,3}; int n=arr.length;
+        System.out.println("Before Sort \n"+Arrays.toString(arr));
 
+        mergeSortFunc(arr,0,n-1);
     }
     
 }
