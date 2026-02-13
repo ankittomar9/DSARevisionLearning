b18e1617d1e714d85de140d0abda029fb0818f20
tomar <tomarankit78@gmail.com>
2026-02-13 05:39:49 +0530

    New Solved Questions

---
+++ java_interviews/companies_based_questions/solutions/Q_1_Check_if_number_is_Prime.java
@@ -0,0 +1,28 @@
+package java_interviews.companies_based_questions.solutions;
+public class Q_1_Check_if_number_is_Prime {
+    public static void main(String args[]){
+        int n=19; 
+        /*Prime number is a number which has exactly two factors 
+         1 and the number itself */
+        boolean isPrime=primeHelper(n);
+        if(isPrime){
+            System.out.println("Given number is not prime : \n "+n);
+        }else{
+             System.out.println("Given number is  prime : \n "+n);
+        }
+    }
+    public static boolean primeHelper(int n){
+        if(n<=0){ System.out.println("Invalid Input or Negative number(Invalid input)"); 
+            return false;}
+
+        int factorsCount=1; // for debug purpose
+        for(int i=2;i<n;i++){  // why not i<=n because n/n which is true
+            if(n%i==0){
+                factorsCount++;
+                return false;
+            }
+        }
+       return true;
+    }
+    
+}
