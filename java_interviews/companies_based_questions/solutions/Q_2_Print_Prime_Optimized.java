83f6c6dd774e0b62db7c2422ffdbb5cac9d7c0dd
tomar <tomarankit78@gmail.com>
2026-02-13 06:09:50 +0530

    New Solved Questions

---
+++ java_interviews/companies_based_questions/solutions/Q_2_Print_Prime_Optimized.java
@@ -0,0 +1,34 @@
+package java_interviews.companies_based_questions.solutions;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class Q_2_Print_Prime_Optimized {
+    public static void main(String args[]){
+        int n=50; //Find all primes up to  50
+        printPrimeSieve(n);
+         }
+         public static void printPrimeSieve(int n){
+            boolean[] isPrime=new boolean[n+1];
+
+            Arrays.fill(isPrime, true);
+            isPrime[0]=false;
+            isPrime[1]=false;
+
+            for(int p=2;p*p<=n;p++){
+                
+                if(isPrime[p]==true){
+
+                    for(int i=p*p;i<=n;i=i+p){
+                        isPrime[i]=false;
+                    }
+                }
+            }
+            List<Integer> primeNumbers=new ArrayList<>();
+            for(int i=2;i<=n;i++){
+                if(isPrime[i]==true){
+                    primeNumbers.add(i);
+                }
+            }
+            System.out.println("Primes up to " + n + ": " + primeNumbers);
+         }    
+}
