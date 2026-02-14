5d01e7faa31497834df47f0b28958b5fa33d87c9
tomar <tomarankit78@gmail.com>
2026-02-14 16:13:49 +0530

    New Solved Questions

---
+++ java_interviews/companies_based_questions/solutions/Q_10_Check_if_a_number_is_a_Strong_Number.java
@@ -0,0 +1,16 @@
+package java_interviews.companies_based_questions.solutions;
+
+public class Q_10_Check_if_a_number_is_a_Strong_Number {
+    /*A number is called a Strong Number (or Krishnamurthy Number)
+     if the sum of the factorials of its digits equals the number itself.
+     Example (145):1! = 1 , 4! = 24 , 5! = 120 , Sum: 1 + 24 + 120 = 145. ) */
+    public static void main(String args[]){
+        int n=145;
+        strongNumberChecker(n);
+    }
+
+    public static void strongNumberChecker(int n){
+        
+    }
+    
+}
