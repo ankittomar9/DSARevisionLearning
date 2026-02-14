823f054c5812cd75037c8202617a4130b6d464b7
tomar <tomarankit78@gmail.com>
2026-02-14 23:02:18 +0530

    New Solved Questions

---
+++ dsa_3_day_spaced_revision/sorting/Q_4_Quick_Sort.java
@@ -0,0 +1,35 @@
+package dsa_3_day_spaced_revision.sorting;
+import java.util.Arrays;
+public class Q_4_Quick_Sort {
+    public static void main(String args[]){
+        int arr[]={10,80,30,90,40}; int n=arr.length;
+        System.out.println("Original Array : \n"+Arrays.toString(arr));
+        quickSort(arr,0,n-1);
+        System.out.println("\nSorted Array : \n"+Arrays.toString(arr));
+    }
+    public static void quickSort(int arr[],int low,int high){
+        if(low<high){
+            int pivotIndex=partition(arr,low,high);
+            quickSort(arr,low,pivotIndex-1);
+            quickSort(arr,pivotIndex+1,high);
+        }
+    }
+        public static int partition(int arr[],int low,int high){
+            int pivot=arr[high];
+            int  pIndex=low;
+
+            for(int j=low;j<high;j++){
+                if(arr[j]<pivot){
+                    swap(arr, pIndex, j);
+                    pIndex++;
+                }
+            }
+            swap(arr, pIndex, high);
+            return pIndex;
+        }
+        public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }    
+}
