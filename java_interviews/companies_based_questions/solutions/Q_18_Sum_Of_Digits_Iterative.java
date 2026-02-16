2a9d78d08bfac8bf49d7d534a1c349eed6484f09
tomar <tomarankit78@gmail.com>
2026-02-16 11:38:55 +0530

    New Solved Questions

---
+++ java_interviews/companies_based_questions/solutions/Q_18_Sum_Of_Digits_Iterative.java
@@ -0,0 +1,26 @@
+package java_interviews.companies_based_questions.solutions;
+
+public class Q_18_Sum_Of_Digits_Iterative {
+    public static void main(String args[]){
+        int n=153;
+        sumDigitsHelper(n);
+
+    }
+    public static void sumDigitsHelper(int n){
+         n=Math.abs(n); // for negative test case
+        if(n==0){
+            System.out.println(0);return;        }
+
+        int sum=0;
+        while(n>0){
+              int extract=n%10;
+              sum=sum+extract;
+
+              n=n/10;
+        }
+      System.out.println("Sum of Digits is : \n"+sum);
+
+
+    }
+    
+}
