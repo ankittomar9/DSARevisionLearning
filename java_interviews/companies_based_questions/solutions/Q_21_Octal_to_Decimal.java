68f245a01c544bbea230544bbdcb554463a26d9b
tomar <tomarankit78@gmail.com>
2026-02-17 22:48:57 +0530

    New Solved Questions

--- java_interviews/companies_based_questions/solutions/Q_21_Octal_to_Decimal.java
+++ java_interviews/companies_based_questions/solutions/Q_21_Octal_to_Decimal.java
@@ -2,8 +2,11 @@ package java_interviews.companies_based_questions.solutions;
 
 public class Q_21_Octal_to_Decimal {
     public static void main(String args[]){
-        int octal=157;
-
+        int octal=157; //Expected Output // 111 
+        /*- **Input:** `157` (Octal)
+        - **Calculation: (1 * 8^2) + (5 *  8^1) + (7  *  8^0)
+        - **Calculation: (1 *  64) + (5 * 8) + (7 * 1)
+         - **Output:** `111` (Decimal) */
         int result= octalToDecimalHelper(octal);
         System.out.println("Decimal of " + octal + " is: " + result);
     }
