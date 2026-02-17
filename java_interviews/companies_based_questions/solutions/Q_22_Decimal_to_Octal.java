256375413098ec17f63cf1908b770872e07428d9
tomar <tomarankit78@gmail.com>
2026-02-17 22:49:00 +0530

    New Solved Questions

--- java_interviews/companies_based_questions/solutions/Q_22_Decimal_to_Octal.java
+++ java_interviews/companies_based_questions/solutions/Q_22_Decimal_to_Octal.java
@@ -1,10 +1,14 @@
 package java_interviews.companies_based_questions.solutions;
-
 public class Q_22_Decimal_to_Octal {
     public static void main(String args[]){
-        int n=111;
+        int decimal=111;
         // output Octal = 157
-        decimalToOctalHelper(n);
+        /*Convert a Decimal number to Octal.
+        - **Input:** `111` (Decimal)
+        - **Process:** Divide by 8, keep remainders.
+        - **Output:** `157` (Octal) */
+
+        decimalToOctalHelper(decimal);
     }
     public static void decimalToOctalHelper(int n){
         long octal=0; int original_Number=n;
@@ -13,12 +17,10 @@ public class Q_22_Decimal_to_Octal {
              int remainder = n%8;
 
             octal=octal+(remainder*place);
-
             n=n/8;
 
             place=place*10;
         }
-       
-        System.out.println("Decimal of  " + original_Number +  " is : "+octal);
+               System.out.println("Octal of  " + original_Number +  " is : "+octal);
     }
 }
