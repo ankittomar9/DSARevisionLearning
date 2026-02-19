a14d3ad38552248c44ac6fb6c251aa37860ef1e4
tomar <tomarankit78@gmail.com>
2026-02-19 21:50:35 +0530

    New Solved Questions

--- java_interviews/companies_based_questions/solutions/Q_10_Check_if_a_number_is_a_Strong_Number.java
+++ java_interviews/companies_based_questions/solutions/Q_10_Check_if_a_number_is_a_Strong_Number.java
@@ -1,5 +1,4 @@
 package java_interviews.companies_based_questions.solutions;
-
 public class Q_10_Check_if_a_number_is_a_Strong_Number {
     /*A number is called a Strong Number (or Krishnamurthy Number)
      if the sum of the factorials of its digits equals the number itself.
@@ -8,7 +7,6 @@ public class Q_10_Check_if_a_number_is_a_Strong_Number {
         int n=145; // check for other numbers as well 
         strongNumberChecker(n);
     }
-
     public static void strongNumberChecker(int n){
         if (n == 0) {
         System.out.println(n + " is NOT a Strong Number");       return;    }
@@ -25,15 +23,12 @@ public class Q_10_Check_if_a_number_is_a_Strong_Number {
        
             n=n/10;
        }
-
        if (sum == originalNumber) {
             System.out.println(originalNumber + " is a Strong Number");
         } else {
             System.out.println(originalNumber + " is NOT a Strong Number");
         }
-
     }
-    
 }
 
 /*TC : O(log(n))
