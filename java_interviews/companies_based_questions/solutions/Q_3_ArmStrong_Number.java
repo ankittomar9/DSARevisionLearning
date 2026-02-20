4b1fb780e500d75f01c9856fcc110ae44f197b0f
tomar <tomarankit78@gmail.com>
2026-02-20 21:54:57 +0530

    New Solved Questions

--- java_interviews/companies_based_questions/solutions/Q_3_ArmStrong_Number.java
+++ java_interviews/companies_based_questions/solutions/Q_3_ArmStrong_Number.java
@@ -1,10 +1,8 @@
 package java_interviews.companies_based_questions.solutions;
-
 public class Q_3_ArmStrong_Number {
     public static void main(String args[]){
         int n=153; // check for 121 153 
-        boolean result=armsStrongHelper(n);
-        
+        boolean result=armsStrongHelper(n);   
         if(!result){
             System.out.println("Given number is not an armstrong number \n"+n);
         }else{
@@ -14,12 +12,9 @@ public class Q_3_ArmStrong_Number {
     public static boolean armsStrongHelper(int n){
         if(n<=0){        return false;
         }
-            int original_Number=n;
-            int sum=0;
-
+            int original_Number=n;            int sum=0;
             while(n>0){
                 int lastDigit=n%10;
-
                 sum=sum+(lastDigit*lastDigit*lastDigit);
              
                // sum=(int) (sum+Math.pow(lastDigit, n)); 
