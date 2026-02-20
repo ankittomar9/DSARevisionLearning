ee1ff357747ab43f5b7668b98ea5b1eb757a1966
tomar <tomarankit78@gmail.com>
2026-02-20 21:54:46 +0530

    New Solved Questions

--- java_interviews/companies_based_questions/solutions/Q_1_Check_if_number_is_Prime.java
+++ java_interviews/companies_based_questions/solutions/Q_1_Check_if_number_is_Prime.java
@@ -6,16 +6,15 @@ public class Q_1_Check_if_number_is_Prime {
          1 and the number itself */
         boolean isPrime=primeHelper(n);
         if(isPrime){
-            System.out.println("Given number is not prime : \n "+n);
+            System.out.println("Given number is a prime : \n "+n);
         }else{
-             System.out.println("Given number is  prime : \n "+n);
+             System.out.println("Given number is not prime : \n "+n);
         }
     }
     public static boolean primeHelper(int n){
         if(n<=0){ System.out.println("Invalid Input or Negative number(Invalid input)"); 
             return false;}
-
-        int factorsCount=1; // for debug purpose
+        int factorsCount=1; 
         for(int i=2;i<n;i++){  // why not i<=n because n/n which is true
             if(n%i==0){
                 factorsCount++;
