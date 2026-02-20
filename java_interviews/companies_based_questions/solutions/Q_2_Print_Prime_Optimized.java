3455b7e7299e6f6f1f1627e3398d1edfa4dee2a6
tomar <tomarankit78@gmail.com>
2026-02-20 21:54:51 +0530

    New Solved Questions

--- java_interviews/companies_based_questions/solutions/Q_2_Print_Prime_Optimized.java
+++ java_interviews/companies_based_questions/solutions/Q_2_Print_Prime_Optimized.java
@@ -13,15 +13,12 @@ public class Q_2_Print_Prime_Optimized {
         return;
     }
             boolean[] isPrime=new boolean[n+1];
-
             Arrays.fill(isPrime, true);
             isPrime[0]=false;
             isPrime[1]=false;
 
-            for(int p=2;p*p<=n;p++){
-                
+            for(int p=2;p*p<=n;p++){          
                 if(isPrime[p]==true){
-
                     for(int i=p*p;i<=n;i=i+p){
                         isPrime[i]=false;
                     }
