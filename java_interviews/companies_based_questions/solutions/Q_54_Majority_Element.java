da6dc6291b3eab02a9e346c33c7c4bf8f8e8faf9
tomar <tomarankit78@gmail.com>
2026-03-03 19:29:18 +0530

    New Solved Questions

---
+++ java_interviews/companies_based_questions/solutions/Q_54_Majority_Element.java
@@ -0,0 +1,9 @@
+package java_interviews.companies_based_questions.solutions;
+
+public class Q_54_Majority_Element {
+    public static void main(String args[]){
+        int arr[]={2, 2, 1, 1, 1, 2, 2};
+        int arr[]={};
+    }
+    
+}
