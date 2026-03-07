d7e9408097681ce75701de518d34c501a053feb4
tomar <tomarankit78@gmail.com>
2026-03-07 11:27:36 +0530

    New Solved Questions

--- java_interviews/companies_based_questions/solutions/Q_67_Check_Alphabet.java
+++ java_interviews/companies_based_questions/solutions/Q_67_Check_Alphabet.java
@@ -2,15 +2,15 @@ package java_interviews.companies_based_questions.solutions;
 
 public class Q_67_Check_Alphabet {
     public static void main(String args[]) {
-        char test1 = 'K';
-        char test2 = 'm';
-        char test3 = '8';
-        char test4 = '*';
+        char var1 = 'K';
+        char var2 = 'm';
+        char var3 = '8';
+        char var4 = '*';
 
-        System.out.println("'" + test1 + "' is alphabet : " + isAlphabet(test1));
-        System.out.println("'" + test2 + "' is alphabet : " + isAlphabet(test2));
-        System.out.println("'" + test3 + "' is alphabet : " + isAlphabet(test3));
-        System.out.println("'" + test4 + "' is alphabet : " + isAlphabet(test4));
+        System.out.println("'" + var1 + "' is alphabet : " + isAlphabet(var1));
+        System.out.println("'" + var2 + "' is alphabet : " + isAlphabet(var2));
+        System.out.println("'" + var3 + "' is alphabet : " + isAlphabet(var3));
+        System.out.println("'" + var4 + "' is alphabet : " + isAlphabet(var4));
     }
 
     public static boolean isAlphabet(char ch) {
