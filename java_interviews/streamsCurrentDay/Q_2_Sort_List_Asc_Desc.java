1472cd6a0360f767b0668ccd1ab942340cb58b39
ankittomar9 <tomarankt78@gmail.com>
2026-03-10 11:42:30 +0530

    New Solved problems

---
+++ java_interviews/streamsCurrentDay/Q_2_Sort_List_Asc_Desc.java
@@ -0,0 +1,24 @@
+package java_interviews.streamsCurrentDay;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Q_2_Sort_List_Asc_Desc {
+    public static void main(String args[]){
+        List<Integer>list=Arrays.asList(1,6,3,4,8,9,5,6,2,0);
+         System.out.println(list);
+        List<Integer> ascList=list.stream()
+        .sorted(Comparator.naturalOrder())
+        .collect(Collectors.toList());
+        System.out.println(ascList);
+
+
+        List<Integer>descList=list.stream()
+        .sorted(Comparator.reverseOrder())
+        .collect(Collectors.toList());
+        System.out.println(ascList);
+    }
+    
+}
