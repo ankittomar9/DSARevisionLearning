8841db845e86f5d7115ebf5708d8effe040bebbb
tomar <tomarankit78@gmail.com>
2026-03-13 22:00:43 +0530

    New Solved Questions

---
+++ dsa_jan_revision/arrayBasics/Q_2_Arrays_Good_Pair_optimized.java
@@ -0,0 +1,12 @@
+package dsa_jan_revision.arrayBasics;
+
+public class Q_2_Arrays_Good_Pair_optimized {
+    public static void main(String args[]){
+        int arr[]={};
+        int n=arr.length;
+    }
+
+    public static void pairCalCulatorHelper(int arr[]){
+
+    }
+}
