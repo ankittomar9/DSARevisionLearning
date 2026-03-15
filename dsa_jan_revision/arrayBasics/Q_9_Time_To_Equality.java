ff52b3b0a372730f64a912778857f060b00d689c
tomar <tomarankit78@gmail.com>
2026-03-15 19:44:48 +0530

    New Solved Questions

---
+++ dsa_jan_revision/arrayBasics/Q_9_Time_To_Equality.java
@@ -0,0 +1,27 @@
+package dsa_jan_revision.arrayBasics;
+
+public class Q_9_Time_To_Equality {
+    public static void main(String args[]){
+        int arr[]={2,4,1,3,2};
+        int result=solve(arr);
+        System.out.println("Minimum time to equality : "+result);
+    }
+    public static int solve(int arr[]){
+        if(arr==null || arr.length==0){System.out.println("Array is null or Empty"); return 0;}
+
+        int max=Integer.MIN_VALUE;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        int total_time=0;
+         for(int i=0;i<arr.length;i++){
+            total_time=total_time+arr[i];
+         }
+         total_time=total_time-max;
+         return total_time;
+    }
+
+}
