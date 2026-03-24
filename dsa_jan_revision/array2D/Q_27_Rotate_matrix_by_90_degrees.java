fa996ae4ee8513b982ef9825a6f08c3057449ac6
tomar <tomarankit78@gmail.com>
2026-03-24 10:14:14 +0530

    New Solved Questions

---
+++ dsa_jan_revision/array2D/Q_27_Rotate_matrix_by_90_degrees.java
@@ -0,0 +1,16 @@
+package dsa_jan_revision.array2D;
+
+public class Q_27_Rotate_matrix_by_90_degrees {
+   public static void main(String args[]){
+    int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+    rotate90DegreeHelper(arr);
+
+   }
+   public static void rotate90DegreeHelper(int arr[][]){
+    if(arr==null || arr.length==0){return ;}
+    //Step1 : do Matrix tranpose
+
+
+    //Step2 : Reverse the Matrix row by row
+   } 
+}
