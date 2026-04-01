3602a2840ae8b6e43ec2e56b6af1ff4a8b145c89
tomar <tomarankit78@gmail.com>
2026-04-01 23:07:18 +0530

    New Solved Questions

---
+++ java_interviews/companies_based_questions/solutions/Q_116_Nth_Term_GP_Recursion.java
@@ -0,0 +1,17 @@
+package java_interviews.companies_based_questions.solutions;
+
+public class Q_116_Nth_Term_GP_Recursion {
+    public static void main(String args[]){
+        int a=2;
+        int r=3;
+        int N=4;
+
+        System.out.println("The " + N + "th term of the GP is: " + nthTermGP(a, r, N));
+
+    }
+    public static long nthTermGP(int a,int r,int N){
+        if(N==1){return a;}
+
+        return r*nthTermGP(a, r, N-1);
+    }
+}
