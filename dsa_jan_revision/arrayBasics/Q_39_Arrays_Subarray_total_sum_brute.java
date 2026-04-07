32621b583b15e53d0156e27dedc7cd14918ac5b2
tomar <tomarankit78@gmail.com>
2026-04-07 14:33:33 +0530

    New Solved Questions

--- dsa_jan_revision/arrayBasics/Q_39_Arrays_Subarray_total_sum_brute.java
+++ dsa_jan_revision/arrayBasics/Q_39_Arrays_Subarray_total_sum_brute.java
@@ -1,11 +1,8 @@
 package dsa_jan_revision.arrayBasics;
-
 public class Q_39_Arrays_Subarray_total_sum_brute {
     public static void main(String args[]){
-
         int arr[]={1,2,3,4,5};
         totalSubarraySumHelper(arr);
-
     }
     public static void totalSubarraySumHelper(int arr[]){
         if(arr==null || arr.length==0) return;
@@ -19,10 +16,9 @@ public class Q_39_Arrays_Subarray_total_sum_brute {
                     sum=sum+arr[k];
                 }
                       totalSum=totalSum+sum;
-                  System.out.println("Subarray (" + i + "," + j + ") sum: " + sum);
+             System.out.println("Subarray (" + i + "," + j + ") sum: " + sum);
             }
         }
           System.out.println("Total Sum is : \n"+totalSum);
-
     }
 }
