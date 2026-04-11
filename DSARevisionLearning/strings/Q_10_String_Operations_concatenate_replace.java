914f3d9c92bad90b835db8dbfd083017b2d7a88e
tomar <tomarankit78@gmail.com>
2026-04-11 23:08:35 +0530

    New Solved Questions

--- DSARevisionLearning/strings/Q_10_String_Operations_concatenate_replace.java
+++ DSARevisionLearning/strings/Q_10_String_Operations_concatenate_replace.java
@@ -1,8 +1,12 @@
 package DSARevisionLearning.strings;
 public class Q_10_String_Operations_concatenate_replace {
     public static void main(String args[]){
+         /*Given a string A, concatenate it with itself to form a new string. From this new string,
+  remove all uppercase letters and replace every vowel ('a', 'e', 'i', 'o', 'u') with '#'. */
         String s1="AbcaZeoB"; System.out.println("Original String \n"+s1);
         stringOperationsConcatReplaceHelper(s1);
+         //Expected output AbcaZeoB+AbcaZeoB = AbcaZeoBAbcaZeoB
+         //  final String         //  bc###bc###  
     }
     public static void stringOperationsConcatReplaceHelper(String str){
         if(str==null || str.isEmpty()){System.out.println("String is empty or null"); return;}
@@ -11,7 +15,6 @@ public class Q_10_String_Operations_concatenate_replace {
 
         for(int i=0;i<str.length();i++){
             char c=str.charAt(i);
-
             if(c>='A' && c<='Z'){
                 continue;
             }
@@ -22,7 +25,6 @@ public class Q_10_String_Operations_concatenate_replace {
             }
         }
             System.out.println("\n New Modified String \n"+sb);
-
             String newString=sb.toString();
             String finalString=newString+newString;
              System.out.println("\nNew Modified String Final Concatenated \n"+finalString);
