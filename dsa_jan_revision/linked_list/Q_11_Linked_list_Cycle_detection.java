5dd1b30ee51160b668b375494b398485711aaead
tomar <tomarankit78@gmail.com>
2026-04-22 23:33:53 +0530

    New Solved Questions

--- dsa_jan_revision/linked_list/Q_11_Linked_list_Cycle_detection.java
+++ dsa_jan_revision/linked_list/Q_11_Linked_list_Cycle_detection.java
@@ -1,13 +1,16 @@
 package dsa_jan_revision.linked_list;
-
 public class Q_11_Linked_list_Cycle_detection {
     public static void main(String args[]){
         /*LeetCode 141 (Easy/Medium)We need to check if a Linked List has a loop (a cycle).
         Normal List: 1 -> 2 -> 3 -> null (Ends) Cyclic List: 1 -> 2 -> 3 -> 2... (Infinite loop)  */
-     Node head = new Node(10);head.next= new Node(20);head.next.next=new Node(30);        head.next.next.next.next=new Node(40) ;
-                  head.next.next.next.next.next=head.next; // connecting 20 to check cycle detection
-          System.out.println("Original List : "); printList(head);  System.out.println();
-          boolean cycleFound=cycleDetectionHelper(head);
+     Node head1 = new Node(10);
+     head1.next= new Node(20);
+     head1.next.next=new Node(30);
+      head1.next.next.next=new Node(40); 
+      head1.next.next.next.next=head1.next; // connecting 20 to check cycle detection
+     //   System.out.println("Original List : "); printList(head);  System.out.println(); this creates infinte loop
+        System.out.println("Testing Cyclic List:");
+     boolean cycleFound=cycleDetectionHelper(head1);
           if(cycleFound) {
             System.out.println("Cycle Detected! The loop exists.");
         } else {
@@ -16,14 +19,11 @@ public class Q_11_Linked_list_Cycle_detection {
     }
     public static boolean cycleDetectionHelper(Node head){
         Node slow= head; Node fast= head;
-        boolean cycleFound=false;
         while(fast!=null && fast.next!=null ){
             slow=slow.next;
             fast=fast.next.next;
             if(slow == fast){
-                return cycleFound=true; 
-            }
-        }
+                return true;             }        }
         return false;
     }
     public static void printList(Node head){
