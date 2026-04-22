6085f6d22c7a23e522330a32a322f96700eb18dd
tomar <tomarankit78@gmail.com>
2026-04-22 22:37:29 +0530

    New Solved Questions

---
+++ dsa_jan_revision/linked_list/Q_10_Reverse_a_LinkedList.java
@@ -0,0 +1,14 @@
+package dsa_jan_revision.linked_list;
+
+public class Q_10_Reverse_a_LinkedList {
+    public static void main(String args[]){
+
+    }
+    public static  class Node{
+        int data;        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+}
