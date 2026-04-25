e79dbb281b4c685c6f6205b03d6953495d55b26d
tomar <tomarankit78@gmail.com>
2026-04-25 22:35:02 +0530

    New Solved Questions

---
+++ dsa_jan_revision/linked_list/Q_26_Add_Two_Numbers.java
@@ -0,0 +1,10 @@
+package dsa_jan_revision.linked_list;
+
+public class Q_26_Add_Two_Numbers {
+    public static void main(String[] args) {
+        
+    }
+    public static Node addTwoNumbers(Node l1,Node l2){
+        
+    }
+}
