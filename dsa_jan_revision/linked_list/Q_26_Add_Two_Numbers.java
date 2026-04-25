873a07a5c2d30e0b96f0b9b1630befea1f879abd
tomar <tomarankit78@gmail.com>
2026-04-25 22:36:09 +0530

    New Solved Questions

--- dsa_jan_revision/linked_list/Q_26_Add_Two_Numbers.java
+++ dsa_jan_revision/linked_list/Q_26_Add_Two_Numbers.java
@@ -5,6 +5,11 @@ public class Q_26_Add_Two_Numbers {
         
     }
     public static Node addTwoNumbers(Node l1,Node l2){
-        
+
     }
+public static void printList(Node head){
+Node temp=head;while(temp!=null){System.out.print(temp.data+ " -> ");temp=temp.next;} 
+System.out.println("null");}
+ public static class Node{
+int data;  Node next;      public Node(int data){ this.data=data; this.next=null;} }    
 }
