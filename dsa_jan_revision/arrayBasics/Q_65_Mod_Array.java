540a384ce9a9c9dd1d15ed8b1ef21c2785140dd7
tomar <tomarankit78@gmail.com>
2026-04-28 19:24:11 +0530

    New Solved Questions

--- dsa_jan_revision/arrayBasics/Q_65_Mod_Array.java
+++ dsa_jan_revision/arrayBasics/Q_65_Mod_Array.java
@@ -1,5 +1,27 @@
 package dsa_jan_revision.arrayBasics;
 
+import java.util.Arrays;
+
 public class Q_65_Mod_Array {
-    
+    public static void main(String[] args) {
+        int arr1[]={1,4,3};
+        int B1=2;
+        System.out.println("Original Array \n"+Arrays.toString(arr1)+" % 2");
+        System.out.println("Output : "+solve(arr1,B1));
+        
+        int arr2[]={4,3,5,3,5,3,2,1};
+        int B2=47;
+        System.out.println("\nOriginal Array \n"+Arrays.toString(arr2)+" % 2");
+        System.out.println("Output : "+solve(arr2,B2));
+        
+    }
+    public static int solve(int arr[],int B){
+        if(arr==null || arr.length==0){return 0;}
+        long ans=0;
+        for(int i=0;i<arr.length;i++){
+            long currentNumber=(ans*10)+arr[i];
+            ans=currentNumber%B;
+        }
+        return (int) ans;
+    }
 }
