13961e18cce4f1e679f5c107e7ac28b6e1990efb
tomar <tomarankit78@gmail.com>
2026-05-04 18:37:59 +0530

    New Solved Questions

--- dsa_jan_revision/bitmanipulation/Q_11_Unset_bits_from_right.java
+++ dsa_jan_revision/bitmanipulation/Q_11_Unset_bits_from_right.java
@@ -5,7 +5,8 @@ public class Q_11_Unset_bits_from_right {
     long A=25; long B=3;    
     /*Problem Statement Given an integer A and an integer B.
         Unset the last B bits of A (make them 0). */
-        //we want to to turn the last three bits of 25 to zero final answer (11000) 001 to 000
+ //we want to to turn the last three bits of 25 to zero 
+ // final answer (11000) 001 to 000
         //25 Binary(168421 this is conversion code) -> (11001) 
         //Binary of 3 (8421) -> (0011)
          System.out.println("Evaluating n : "+A+ " and Position i : "+B);
