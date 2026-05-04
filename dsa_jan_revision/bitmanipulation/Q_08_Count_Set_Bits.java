1ca0532b9dec1c85cdb2b9679f335bec13766d1f
tomar <tomarankit78@gmail.com>
2026-05-04 18:37:54 +0530

    New Solved Questions

--- dsa_jan_revision/bitmanipulation/Q_08_Count_Set_Bits.java
+++ dsa_jan_revision/bitmanipulation/Q_08_Count_Set_Bits.java
@@ -11,7 +11,6 @@ public class Q_08_Count_Set_Bits {
             System.out.println("Evaluating: 11 (Binary: -1)");
             // binary is whole integer  of negative number (Binary: 1111...1111)
         System.out.println("Set bits counted: " + solve(-1) + "\n");
-        
     }
     public static int solve(int n){
         int count=0;
