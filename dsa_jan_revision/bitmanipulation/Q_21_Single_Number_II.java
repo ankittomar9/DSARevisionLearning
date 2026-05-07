1abba7fc1712a08bcd704c70c08ca1b8c3fb12eb
tomar <tomarankit78@gmail.com>
2026-05-07 21:13:55 +0530

    New Solved Questions

--- dsa_jan_revision/bitmanipulation/Q_21_Single_Number_II.java
+++ dsa_jan_revision/bitmanipulation/Q_21_Single_Number_II.java
@@ -1,21 +1,17 @@
 package dsa_jan_revision.bitmanipulation;
-
 import java.util.Arrays;
-
 public class Q_21_Single_Number_II {
     public static void main(String[] args) {
         int arr1[]={5,5,5,2}; System.out.println("Original Array : \n"+Arrays.toString(arr1));
          System.out.println("Unique Number is: " + solve(arr1) + "\n");
 
          int arr2[]= {-2, -2, 1, 1, 4, 1, 4, 4, -4, -2};
-          System.out.println("Original Array : \n"+Arrays.toString(arr1));
+          System.out.println("Original Array : \n"+Arrays.toString(arr2));
          System.out.println("Unique Number is: " + solve(arr2) + "\n");
-
     }
     public static int solve (int arr[]){
         if(arr==null || arr.length==0){System.out.println("empty or Null array \n");return 0;}
         int ans=0;
-
         for(int i=0;i<32;i++){
             int count =0;
 
@@ -24,11 +20,8 @@ public class Q_21_Single_Number_II {
                     count++;
                 }
             }
-
             if(count %3 !=0){
                 ans= ans | (1<<i);
             }
         }
-        return ans;
-    }
-}
+        return ans;    }    }
