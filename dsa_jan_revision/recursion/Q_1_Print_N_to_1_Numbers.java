e9fc648f7100697a4d9bb4b143f4fd2dceb8fab7
tomar <tomarankit78@gmail.com>
2026-05-08 19:48:35 +0530

    New Solved Questions

---
+++ dsa_jan_revision/recursion/Q_1_Print_N_to_1_Numbers.java
@@ -0,0 +1,17 @@
+package dsa_jan_revision.recursion;
+
+public class Q_1_Print_N_to_1_Numbers {
+    public static void main(String[] args) {
+        int n=10;
+          System.out.print(" Printing Numbers from "+n+" to 1 \n");
+        printN_To_1_Numbers_Helper(n);
+    }
+    public static void printN_To_1_Numbers_Helper(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print(" "+n);
+        printN_To_1_Numbers_Helper(n-1);
+
+    }
+}
