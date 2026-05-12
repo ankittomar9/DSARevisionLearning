55776810e04cc1962f1f97e3cc115751998d897c
tomar <tomarankit78@gmail.com>
2026-05-12 18:42:07 +0530

    New Solved Questions

--- dsa_jan_revision/Tree/Q_3_Total_Nodes_in_Tree.java
+++ dsa_jan_revision/Tree/Q_3_Total_Nodes_in_Tree.java
@@ -2,32 +2,27 @@ package dsa_jan_revision.Tree;
 
 public class Q_3_Total_Nodes_in_Tree {
     public static void main(String[] args) {
-        // Build the Tree: 50(root), 30(L), 70(R), 20(LL), 40(LR)
-        TreeNode root1=new TreeNode(50);    root1.left=new TreeNode(30);
-        root1.right=new TreeNode(70);       root1.left.left=new TreeNode(20);
-        root1.left.right=new TreeNode(40);
+       // Build the Tree: 50(root), 30(L), 70(R), 20(LL), 40(LR)
+            TreeNode root=new TreeNode(50);
+            root.left=new TreeNode(30);
+            root.right=new TreeNode(70);
+            root.left.left=new TreeNode(20);
+            root.left.right=new TreeNode(40);    
 
-        // Build the Tree Similar : 50(root), 30(L), 70(R), 20(LL), 40(LR)
-        TreeNode root2=new TreeNode(50); root2.left=new TreeNode(30);
-        root2.right=new TreeNode(70);    root2.left.left=new TreeNode(20);
-        root2.left.right=new TreeNode(40);
-
-
-
+         System.out.println("Tree constructed.");
+        System.out.println("Calculating nodes Count.");
+            int result=NodesCounter(root);
+            System.out.println("Total Nodes are : \n "+result);
     }
-    public static boolean isTreeIdentical(TreeNode root1,TreeNode root2){
-        if(root1==null && root2==null){
-            return true;
-        }
+    public static int NodesCounter(TreeNode root){
+        if(root==null ){return 0;}
+        int leftCount=NodesCounter(root.left);
+        int rightCount=NodesCounter(root.right);
+        return leftCount+rightCount+1;
     }
-    public static class TreeNode{
-        int data;
-        TreeNode left;
-        TreeNode right;
-            public TreeNode(int data){
-            this.data=data;
-            this.left=null;
-            this.right=null;
-        }
+     public static class TreeNode{
+        int data;        TreeNode left;         TreeNode right;
+        public TreeNode(int data){ this.data=data;    this.left=null;  this.right=null; }
     }
+
 }
